d486023ab922d5821aeeeb42115763b3e3d27546
e1732a364fed <75717694+e1732a364fed@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    为darwin和windows添加bindToDevice功能;修订文档

--- netLayer/sockopt_other.go
+++ netLayer/sockopt_other.go
@@ -1,8 +1,8 @@
-//go:build !linux
-// +build !linux
+//go:build !(linux || darwin || windows)
+// +build !linux,!darwin,!windows
 
 package netLayer
 
-//SetSockOpt 是平台相关的.
+// SetSockOpt 是平台相关的.
 func SetSockOpt(fd int, sockopt *Sockopt, isudp bool, isipv6 bool) {
 }
