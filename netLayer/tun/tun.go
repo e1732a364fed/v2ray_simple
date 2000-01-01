5f46d7b1e381b75d9f580852b559188f390068f6
e1732a364fed <75717694+e1732a364fed@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    将tun的ExampleTest移动到test.go中

--- netLayer/tun/tun.go
+++ netLayer/tun/tun.go
@@ -19,48 +19,10 @@ package tun
 
 import (
 	"io"
-	"log"
-	"net"
-	"net/http"
-	"net/netip"
 
 	"github.com/e1732a364fed/v2ray_simple/netLayer"
 	"github.com/e1732a364fed/v2ray_simple/utils"
-	"golang.zx2c4.com/wireguard/conn"
-	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun/netstack"
 )
-
-func ExampleTest() {
-	tun, tnet, err := netstack.CreateNetTUN(
-		[]netip.Addr{netip.MustParseAddr("192.168.4.29")},
-		[]netip.Addr{netip.MustParseAddr("8.8.8.8"), netip.MustParseAddr("8.8.4.4")},
-		1420,
-	)
-	if err != nil {
-		log.Panic(err)
-	}
-	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
-	dev.IpcSet(`private_key=a8dac1d8a70a751f0f699fb14ba1cff7b79cf4fbd8f09f44c6e6a90d0369604f
-public_key=25123c5dcd3328ff645e4f2a3fce0d754400d3887a0cb7c56f0267e20fbf3c5b
-endpoint=163.172.161.0:12912
-allowed_ip=0.0.0.0/0
-persistent_keepalive_interval=25
-`)
-	dev.Up()
-	listener, err := tnet.ListenTCP(&net.TCPAddr{Port: 80})
-	if err != nil {
-		log.Panicln(err)
-	}
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		log.Printf("> %s - %s - %s", request.RemoteAddr, request.URL.String(), request.UserAgent())
-		io.WriteString(writer, "Hello from userspace TCP!")
-	})
-	err = http.Serve(listener, nil)
-	if err != nil {
-		log.Panicln(err)
-	}
-}
 
 // selfaddr是tun向外拨号时使用的ip; realAddr 是 tun接收数据时对外暴露的ip。也被称为gateway
 // realAddr 是在路由表中需要配置的那个ip。
