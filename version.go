52f12d99c04513cc75f38b610d407fe8d0b545fd
hahahrfool <75717694+hahahrfool@users.noreply.github.com>
2022-03-14 11:43:32 +0800

    更新文档; update version.go

--- version.go
+++ version.go
@@ -8,6 +8,6 @@ import (
 var Version string //版本号由Makefile指定
 
 func printVersion() {
-	fmt.Printf("===============================\nv2ray_simple %v (%v), %v %v %v\n", Version, desc, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("===============================\nverysimple %v (%v), %v %v %v\n", Version, desc, runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
 }
